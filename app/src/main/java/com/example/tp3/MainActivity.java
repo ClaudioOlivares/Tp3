4a4b9e01c58e328113fa16a80b28fa9f19cedf68
ClaudioOlivares <claudio_05_028@hotmail.com>
2020-04-13 02:59:42 -0300

    Initial commit

---
+++ app/src/main/java/com/example/tp3/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.tp3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+private ArrayList<Inmueble> lista = new ArrayList<>();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        cargarDatos();
+        generarListView();
+    }
+
+    public void generarListView()
+    {
+        ArrayAdapter<Inmueble> adapter = new ListaAdapter(this,R.layout.item,lista,getLayoutInflater());
+        ListView lv = findViewById(R.id.milista);
+        lv.setAdapter(adapter);
+    }
+    public void cargarDatos()
+    {
+        lista.add(new Inmueble(R.drawable.caba,"Juana Koslay",80000));
+        lista.add(new Inmueble(R.drawable.caba2,"Cuchi Corral",180000));
+        lista.add(new Inmueble(R.drawable.caba3,"Chosme",233555));
+
+    }
+}
